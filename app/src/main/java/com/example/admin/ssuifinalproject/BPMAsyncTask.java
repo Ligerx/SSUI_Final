140815572d08f7bbdbfd2f9d24659a23f80d3fea
awang1 <awang1@andrew.cmu.edu>
2015-12-04 02:22:36 -0500

    Update the results activity, its XML, and the data shown

--- app/src/main/java/com/example/admin/ssuifinalproject/BPMAsyncTask.java
+++ app/src/main/java/com/example/admin/ssuifinalproject/BPMAsyncTask.java
@@ -7,56 +7,60 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 
 public class BPMAsyncTask extends AsyncTask<String, Void, BeatAnalyzer> {
 
     private Context context;
-    private Chart chart;
     private LinearLayout loading;
-//    private TextView textView;
+    private LinearLayout resultsContainer;
+    private Chart chart;
 
     public BPMAsyncTask(Context context) {
         this.context = context;
-        this.chart = (Chart) ((Activity) context).findViewById(R.id.chart);
         this.loading = (LinearLayout) ((Activity) context).findViewById(R.id.loading_placeholder);
-//        this.textView = (TextView) ((Activity) context).findViewById(R.id.textView);
+        this.resultsContainer = (LinearLayout) ((Activity) context).findViewById(R.id.results_container);
+        this.chart = (Chart) ((Activity) context).findViewById(R.id.chart);
     }
 
-//    @Override
-//    protected void onPreExecute() {
-//        super.onPreExecute();
-//        textView.setText("Processing...");
-//    }
-
     protected BeatAnalyzer doInBackground(String... args) {
-//        int count = urls.length;
-//        long totalSize = 0;
-//        for (int i = 0; i < count; i++) {
-//            totalSize += Downloader.downloadFile(urls[i]);
-//            publishProgress((int) ((i / (float) count) * 100));
-//            // Escape early if cancel() is called
-//            if (isCancelled()) break;
-//        }
-//        return totalSize;
-
         // Should be path to audio file
         String path = args[0];
         return new BeatAnalyzer(path, context);
     }
 
     protected void onPostExecute(BeatAnalyzer beatAnalyzer) {
-//        showDialog("Downloaded " + result + " bytes");
-
-//        textView.setText("Median BPM: " + beatAnalyzer.getMedianBPM());
-//
-//        textView.append("\n\nBPM over time:");
-//        for(double bpm : beatAnalyzer.getBPMOverTime()) {
-//            textView.append("\n" + String.valueOf(bpm));
-//        }
-
         loading.setVisibility(View.GONE); // hide the loading spinner
-        chart.setVisibility(View.VISIBLE); // show the chart
+        resultsContainer.setVisibility(View.VISIBLE); // show everything else
+
+        ArrayList<Double> bpm = beatAnalyzer.getBPMOverTime();
 
 
+        ArrayList<Entry> bpmEntries = new ArrayList<>();
+        ArrayList<String> xVals = new ArrayList<>();
+
+        // Add all bpm data points, time independent right now
+        for(int i = 0; i < bpm.size(); i++) {
+            Entry entry = new Entry(bpm.get(i).floatValue(), i);
+            bpmEntries.add(entry);
+
+            xVals.add(String.valueOf(i));
+        }
+
+        LineDataSet bpmLine = new LineDataSet(bpmEntries, "BPM (no time yet)");
+        bpmLine.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(bpmLine);
+
+        LineData lineChart = new LineData(xVals, dataSets);
+
+        chart.setData(lineChart);
+        chart.invalidate(); // (re)draw
     }
 }
