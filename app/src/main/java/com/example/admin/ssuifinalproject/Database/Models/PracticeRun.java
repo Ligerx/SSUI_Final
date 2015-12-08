536f04c5c5a8ba29bb8d0a939702653f8a1bc808
awang1 <awang1@andrew.cmu.edu>
2015-12-07 23:12:22 -0500

    Implement practice run and song model object

--- app/src/main/java/com/example/admin/ssuifinalproject/Database/Models/PracticeRun.java
+++ app/src/main/java/com/example/admin/ssuifinalproject/Database/Models/PracticeRun.java
@@ -1,7 +1,60 @@
 package com.example.admin.ssuifinalproject.Database.Models;
 
-/**
- * Created by admin on 12/7/15.
- */
+import java.util.Date;
+
 public class PracticeRun {
+    private int id;
+    private int song_id;
+    private double targetBPM;
+    private double medianBPM;
+    private Date createdAt;
+
+    public PracticeRun(int id, int song_id, double targetBPM, double medianBPM, int createdAt) {
+        this.id = id;
+        this.song_id = song_id;
+        this.targetBPM = targetBPM;
+        this.medianBPM = medianBPM;
+        this.createdAt = new Date(createdAt); // convert the time in millis to date object
+    }
+
+    //// Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSong_id() {
+        return song_id;
+    }
+
+    public void setSong_id(int song_id) {
+        this.song_id = song_id;
+    }
+
+    public double getTargetBPM() {
+        return targetBPM;
+    }
+
+    public void setTargetBPM(double targetBPM) {
+        this.targetBPM = targetBPM;
+    }
+
+    public double getMedianBPM() {
+        return medianBPM;
+    }
+
+    public void setMedianBPM(double medianBPM) {
+        this.medianBPM = medianBPM;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 }
--- app/src/main/java/com/example/admin/ssuifinalproject/Database/Models/Song.java
+++ app/src/main/java/com/example/admin/ssuifinalproject/Database/Models/Song.java
@@ -1,7 +1,51 @@
 package com.example.admin.ssuifinalproject.Database.Models;
 
-/**
- * Created by admin on 12/7/15.
- */
+import java.util.Date;
+
 public class Song {
+
+    private int id;
+    private String title;
+    private double targetBPM;
+    private Date createdAt;
+
+    public Song(int id, String title, double targetBPM, int createdAt) {
+        this.id = id;
+        this.title = title;
+        this.targetBPM = targetBPM;
+        this.createdAt = new Date(createdAt); // convert the time in millis to date object
+    }
+
+    //// Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getTargetBPM() {
+        return targetBPM;
+    }
+
+    public void setTargetBPM(double targetBPM) {
+        this.targetBPM = targetBPM;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 }
