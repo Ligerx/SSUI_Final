c2d34c9fe3dc29eff7255983f1e5f9f3d7ca5b33
awang1 <awang1@andrew.cmu.edu>
2015-12-07 23:36:21 -0500

    Implement BPM model

--- app/src/main/java/com/example/admin/ssuifinalproject/Database/Models/BPM.java
+++ app/src/main/java/com/example/admin/ssuifinalproject/Database/Models/BPM.java
@@ -1,7 +1,36 @@
 package com.example.admin.ssuifinalproject.Database.Models;
 
-/**
- * I don't believe this model is actually necessary. Leave blank for now.
- */
+import java.util.Date;
+
 public class BPM {
+
+    private int id;
+    private int practiceRun_id;
+    private double time;
+    private Date createdAt;
+
+    public BPM(int id, int practiceRun_id, double time, int createdAt) {
+        this.id = id;
+        this.practiceRun_id = practiceRun_id;
+        this.time = time;
+        this.createdAt = new Date(createdAt); // convert the time in millis to date object
+    }
+
+    //// Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public int getPracticeRun_id() {
+        return practiceRun_id;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
 }
+
